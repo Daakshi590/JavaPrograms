f55d599f5eca7929fe90ce5dea7780674217e262
RRamesh590 <89152024+RRamesh590@users.noreply.github.com>
2021-08-18 22:25:45 +0530

    Adding two floats

--- Welcome.java
+++ Welcome.java
@@ -6,5 +6,9 @@ class HelloWorld
     System.out.println("Welocme to Github");
     int a=5,b=6;
   System.out.println("Addition="+(a+b));
+  
+  float c=3.4,d=4.5;
+  System.out.println("Addition of float values="+(c+d));
+  
     
 }
