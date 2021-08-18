44bb2c529d8508c96c742ecc7c599784aeaa86ac
RRamesh590 <89152024+RRamesh590@users.noreply.github.com>
2021-08-18 22:11:16 +0530

    adding two integers

--- Welcome.java
+++ Welcome.java
@@ -4,5 +4,7 @@ class HelloWorld
  {
    System.out.println("Welcome to Java World");
     System.out.println("Welocme to Github");
- 
+    int a=5,b=6;
+  System.out.println("Addition="+(a+b));
+    
 }
